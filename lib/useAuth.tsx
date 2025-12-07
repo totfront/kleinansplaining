1b8132b514b1e5cedea58b29a812c3638d897afb
Alex Mir <alexmir@Inessas-Air.fritz.box>
2025-12-07 14:13:13 +0100

    remove: auth flow

--- lib/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-// Client auth flow removed. This file intentionally left as a minimal stub
-// so that other code can be changed incrementally without immediately
-// failing imports. If you need to re-enable the auth flow, restore the
-// previous implementation from git history.
-
-export {};
